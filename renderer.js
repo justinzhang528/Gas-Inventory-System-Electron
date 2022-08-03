eacfb9bd86141a3f5c69008c113f605352103edd
Justin <justinzhang515@gmail.com>
2022-08-03 14:20:29 +0630

    refactor autoUpdate code

--- renderer.js
+++ renderer.js
@@ -1,20 +1,4 @@
-// const {ipcRenderer} = require('electron');
-
-// // const closeApp = document.getElementById('exit_btn');
-// // closeApp.addEventListener('click', () => {
-// //     ipcRenderer.send('closeApp ')
-// // });
-
-// function on_exit_func(){
-//     ipcRenderer.send('closeApp')
-// }
-
 function on_exit_func(){
     const {ipcRenderer} = require('electron')
     ipcRenderer.send('closeApp')
  }
-
-
-function on_change_color_func(){
-    $('#heading').css("color","red")
-}
