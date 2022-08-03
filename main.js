38d97d57bd72e244c959f73f350ac30fa0029cb7
Justin <justinzhang515@gmail.com>
2022-08-03 13:37:38 +0630

    fixed error in package.json

--- main.js
+++ main.js
@@ -65,6 +65,7 @@ ipcMain.on('app_version', (event) => {
 autoUpdater.on('update-available', () => {
   mainWindow.webContents.send('update_available');
 });
+
 autoUpdater.on('update-downloaded', () => {
   mainWindow.webContents.send('update_downloaded');
 });
